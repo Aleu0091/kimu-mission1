331ef2cdfc5e64efddfe0d8801c6291165420aaf
Aleu <111565097+Aleu0091@users.noreply.github.com>
2024-01-17 21:48:05 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+
+const token =
+    "MTE5NzAwMjAwMDEzNTYxODY2MA.GeR5I0.0IVJdog7mHS5vFsmOiP_124qR6C8GCxeQXb8CY";
+const clientId = "1197002000135618660";
+const guildId = "1195618199744557106";
+
+const commands = [
+    {
+        name: "안녕",
+        description: "안녕 명령어 테스트",
+    },
+];
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+client.once("ready", () => {
+    console.log(`Logged in as ${client.user?.tag}`);
+
+    const rest = new REST({ version: "9" }).setToken(token);
+
+    (async () => {
+        try {
+            console.log("Started refreshing application (/) commands.");
+
+            await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+                body: commands,
+            });
+
+            console.log("Successfully reloaded application (/) commands.");
+        } catch (error) {
+            console.error(error);
+        }
+    })();
+});
+
+client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === "안녕") {
+        await interaction.reply("안녕하세요 세상이여!");
+    }
+});
+
+client.login(token);
